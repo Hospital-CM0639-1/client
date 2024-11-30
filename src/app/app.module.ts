51d219d325ddc7419b756fd74b1fb30f53227965
stefanozogno <1001900@stud.unive.it>
2024-11-30 12:14:32 +0100

    Initialize Angular project with PrimeNG library

---
+++ src/app/app.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { ButtonModule } from 'primeng/button';
+
+@NgModule({
+  declarations: [],
+
+  imports: [BrowserModule, ButtonModule],
+
+  providers: [],
+
+  bootstrap: [],
+})
+export class AppModule {}
