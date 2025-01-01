b46f282c967b3a32f83cdee0ec92dfe55ad65730
stefanozogno <1001900@stud.unive.it>
2025-01-01 15:24:38 +0100

    route patient

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,4 @@
+import { PatientRoutingModule } from './modules/patient/patient-routing.module';
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
@@ -13,6 +14,13 @@ export const routes: Routes = [
     loadChildren: () =>
       import('./modules/doctor/doctor-routing.module').then(
         (m) => m.DoctorRoutingModule
+      ),
+  },
+  {
+    path: 'patient',
+    loadChildren: () =>
+      import('./modules/patient/patient-routing.module').then(
+        (m) => m.PatientRoutingModule
       ),
   },
   { path: '**', redirectTo: 'access/login' }, // Fallback route
