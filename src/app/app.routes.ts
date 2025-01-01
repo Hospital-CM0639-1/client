156ecbf4e5c1ebf2b06d25a3ed145e1fb0665950
stefanozogno <1001900@stud.unive.it>
2025-01-01 15:18:30 +0100

    router path init

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'access',
+    loadChildren: () =>
+      import('./modules/access/access-routing.module').then(
+        (m) => m.AccessRoutingModule
+      ),
+  },
+  { path: '**', redirectTo: 'access/login' }, // Fallback route
+];
