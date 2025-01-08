f60a96241da49a02b244dfc5053f11435183caf9
stefanozogno <1001900@stud.unive.it>
2025-01-08 17:25:13 +0100

    merge with 01-nurse
    # Conflicts:
    #	src/app/shared/services/api.service.ts
    #	src/proxy.conf.json

--- src/app/modules/access/login/login.component.ts
+++ src/app/modules/access/login/login.component.ts
@@ -2,6 +2,12 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from "@angular/forms";
 import { Router } from "@angular/router";
 import { AuthUserService } from "../../../shared/services/user/auth-user.service";
+import { CommonModule } from '@angular/common';
+import { InputTextModule } from 'primeng/inputtext';
+import { ButtonModule } from 'primeng/button';
+import { ProgressSpinnerModule } from 'primeng/progressspinner';
+import { CardModule } from 'primeng/card';
+import { FloatLabelModule } from 'primeng/floatlabel';
 
 @Component({
   standalone: true,
@@ -9,34 +15,50 @@ import { AuthUserService } from "../../../shared/services/user/auth-user.service
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss',
   imports: [
+    CommonModule,
     ReactiveFormsModule,
-    FormsModule
+    FormsModule,
+    InputTextModule,
+    ButtonModule,
+    ProgressSpinnerModule,
+    CardModule,
+    FloatLabelModule,
   ],
 })
 export class LoginComponent implements OnInit {
   protected loginForm!: FormGroup;
+  protected loading = false; // Controls the loading state
 
   constructor(
-      private fb: FormBuilder,
-      private router: Router,
-      private authUserService: AuthUserService,
-  ) {
-
-  }
-
-  onLogin() {
-    this.authUserService.onLogin(this.loginForm.value).subscribe({
-      next: () => {
-        console.log(this.authUserService.getFirstRouteAfterAuthentication())
-        this.router.navigate([this.authUserService.getFirstRouteAfterAuthentication()]);
-      }
-    });
-  }
+    private fb: FormBuilder,
+    private router: Router,
+    private authUserService: AuthUserService,
+  ) {}
 
   ngOnInit(): void {
     this.loginForm = this.fb.group({
       username: ['admin', [Validators.required]],
       password: ['admin', [Validators.required]],
+    });
+  }
+
+  onLogin() {
+    if (this.loginForm.invalid) {
+      return;
+    }
+
+    this.loading = true; // Show loader
+
+    this.authUserService.onLogin(this.loginForm.value).subscribe({
+      next: () => {
+        this.router.navigate([this.authUserService.getFirstRouteAfterAuthentication()]);
+      },
+      error: () => {
+        this.loading = false; // Hide loader on error
+      },
+      complete: () => {
+        this.loading = false; // Hide loader on completion
+      },
     });
   }
 }
